82c8c2a5abab4bed1a8fd1bf5fa0047bfa4d9ce0
djilanov@gmail.com <djilanov@gmail.com>
2015-11-13 23:53:38 +0200

    Integrate server IP into the standart config

--- js/config.js
+++ js/config.js
@@ -2,9 +2,11 @@ var config = {
 	// we set here the interval of the carousel in the front page
 	carousel_interval: 4000,
 	// office ip
-	api: 'http://192.168.17.82:8080/api/home',
+	// api: 'http://192.168.17.82:8080/api/home',
 	// home ip
 	// api: 'http://192.168.1.3:8080/api/home',
+	// server ip
+	api: 'http://194.79.15.131:8080/api/home',
 	imageFront: '/toni-website/img/',
 	bigImage    : 'big',
 	smallImage	: 'small',
