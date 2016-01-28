ccbbe7a88c91f4a25a684fcc57fe26f032039c36
Dimitar <Dimitar_Jilanov@epam.com>
2016-01-28 17:01:28 +0200

    Integrate loading screen

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// On your marks, get set...
-$(document).ready(function(){
-	// we start the carousel
-	$('.carousel').carousel();
-	implementClickEvents();
-}); // document ready
--- js/site.js
+++ js/site.js
@@ -1,33 +1,6 @@
-/**
- * Copyright (c) 2011 Dimitar Jilanov
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
- *
- * Date: 03 12 2013
- *
- */
- // activated on flag click of the navigation
- function onFlagClick() {
- 	debugger;
- }
- function implementClickEvents(){
- 	$('.flag').click(onFlagClick);
- }
+// function for the loading screen
+window.loading_screen = pleaseWait({
+ logo: "./img/logo2.png",
+ backgroundColor: '#3366ff',
+ loadingHtml: '<div class="loadingText">Loading</div></div><div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
+});
